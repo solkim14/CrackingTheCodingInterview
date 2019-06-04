533c7def449f01423adb592dac2024e0413f7c55
Sol <solkim18@gmail.com>
2019-06-04 09:28:46 -0500

    initialized CheckPermutation and URLify problem

---
+++ 1ArraysAndStrings/CheckPermutation.java
@@ -0,0 +1,26 @@
+/**
+* Give two strings, write a method to decide if one is a permutation of the other
+*
+* @version 1.0
+*/
+
+//imports
+
+public class CheckPermutation {
+
+	/**
+	* //Time Complexity
+	*
+	* Summary:  
+	*			
+	* @param 
+	* @return 
+	*/
+	public static void checkPermutation() {
+		//solution logic
+	}
+
+	public static void main(String[] args) {
+		//function call
+	}
+}
---
+++ 1ArraysAndStrings/URLify.java
@@ -0,0 +1,28 @@
+/**
+* Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the
+* end to hold the additional characters, and that you are given the "true" length of the string. (Note: If implementing in Java,
+* please use a character array so taht you can perform this operation in place.)
+*
+* @version 1.0
+*/
+
+//imports
+
+public class URLify {
+
+	/**
+	* //Time Complexity
+	*
+	* Summary:  
+	*			
+	* @param 
+	* @return 
+	*/
+	public static void function() {
+		//solution logic
+	}
+
+	public static void main(String[] args) {
+		//function call
+	}
+}
